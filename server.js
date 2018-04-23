11a7ebbe44bda56c7810811094b70cd9d00bcb6a
redgemini <wendykiddy@Wendys-MBP.lan1>
2018-04-23 13:19:53 -0500

    addSetUpComments/Code

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//Your server.js file should require the basic npm packages we've used in class: express, body-parser and path
+
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
