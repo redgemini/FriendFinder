6b7ee17fa8c6cb6bb291658fc48a1572b755933c
redgemini <wkiddy@gmail.com>
2018-04-27 17:11:27 -0500

    set up server.js

--- server.js
+++ server.js
@@ -3,3 +3,21 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var path = require("path");
+
+var PORT = process.env.PORT || 80801;
+
+// Sets up Express app to handle data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+//Router path
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//Listner
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
