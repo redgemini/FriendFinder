5ad9f31c65e59c97c9bbf082676da22eb03a785d
redgemini <wkiddy@gmail.com>
2018-04-27 20:22:13 -0500

    update route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,66 +1,61 @@
 //Your apiRoutes.js file should contain two routes:
 //A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-//A POST routes /api/friends. This will be used to handle incoming survey results. 
+//A POST routes /api/friends. This will be used to handle incoming survey results.
 //This route will also be used to handle the compatibility logic.
 
 //Require friends
 var friends = require("../data/friends");
 
 //Route
-module.export = function (app) {
-
-//Get API requests    
-    app.get("/api/friends"), function (req,res){
-        res.json(friends);
-    }
-}
-app.post("/api/friends", function (req, res) {
-    
-    
-    // loop through all of the options for a friend match
-    var friendMatch = {
-      name: "",
-      photo: "",
-      friendDifference: 
+module.export = function(app) {
+  //Get API requests
+  app.get("/api/friends"),
+    function(req, res) {
+      res.json(friends);
     };
+};
+app.post("/api/friends", function(req, res) {
+  // loop through all of the options for a friend match
+  var friendMatch = {
+    name: "",
+    photo: "",
+    friendDifference: Infinity
+  };
 
-    // Here we take the result of the user"s survey POST and parse it.
-    var userData = req.body;
-    var userScores = userData.scores;
+  // Pull in the user"s survey POST and parse it.
+  var userData = req.body;
+  var userScores = userData.scores;
 
-    // This variable will calculate the difference between the user"s scores and the scores of
-    // each user in the database
-    var totalDifference;
+  var totalDifference;
 
-    // Here we loop through all the friend possibilities in the database.
-    for (var i = 0; i < friends.length; i++) {
-      var currentFriend = friends[i];
-      totalDifference = 0;
+  // loop through database
+  for (var i = 0; i < friends.length; i++) {
+    var currentFriend = friends[i];
+    totalDifference = 0;
 
-      console.log(currentFriend.name);
+    console.log(currentFriend.name);
 
-      // We then loop through all the scores of each friend
-      for (var j = 0; j < currentFriend.scores.length; j++) {
-        var currentFriendScore = currentFriend.scores[j];
-        var currentUserScore = userScores[j];
+    // loop through all the scores of each friend
+    for (var j = 0; j < currentFriend.scores.length; j++) {
+      var currentFriendScore = currentFriend.scores[j];
+      var currentUserScore = userScores[j];
 
-        // We calculate the difference between the scores and sum them into the totalDifference
-        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
-      }
-
-      // If the sum of differences is less then the differences of the current "friend match"
-      if (totalDifference <= friendMatch.friendDifference) {
-        // Reset the friendMatch to be the new friend.
-        friendMatch.name = currentFriend.name;
-        friendMatch.photo = currentFriend.photo;
-        friendMatch.friendDifference = totalDifference;
-      }
+      // the totalDifference
+      totalDifference += Math.abs(
+        parseInt(currentUserScore) - parseInt(currentFriendScore)
+      );
     }
+    // If the sum of differences is less then the differences of the current "friend match"
+    if (totalDifference <= friendMatch.friendDifference) {
+      // Reset the friendMatch to be the new friend.
+      friendMatch.name = currentFriend.name;
+      friendMatch.photo = currentFriend.photo;
+      friendMatch.friendDifference = totalDifference;
+    }
+  }
 
-    
-    friends.push(userData);
+  friends.push(userData);
 
-    // Return a JSON with the friendMatch. 
-    res.json(friendMatch);
-  });
-
+  // Return a JSON with the friendMatch.
+  res.json(friendMatch);
+});
